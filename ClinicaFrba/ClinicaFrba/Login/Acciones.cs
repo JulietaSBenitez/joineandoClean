02e611382d0d9c03e23f3d64eee55a199f544a56
Nicolas <nicogon@Nicolass-MacBook.local>
2016-11-04 10:13:17 -0400

    login pantallas

---
+++ ClinicaFrba/ClinicaFrba/Login/Acciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicaFrba.Login
+{
+    public partial class Acciones : Form
+    {
+        public Acciones()
+        {
+            InitializeComponent();
+        }
+    }
+}
