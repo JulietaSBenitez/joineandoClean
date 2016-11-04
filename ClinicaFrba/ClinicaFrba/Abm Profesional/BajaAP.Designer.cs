558b4bba06c2d935fcd46dba7f6134b40f4dd862
MagdalenaMileto <maggiemileto@gmail.com>
2016-11-04 12:32:21 -0300

    Probando

---
+++ ClinicaFrba/ClinicaFrba/Abm Profesional/BajaAP.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ClinicaFrba.Abm_Profesional
+{
+    partial class BajaAP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.AgendaTabla = new System.Windows.Forms.DataGridView();
+            this.NombreMedicoLabel = new System.Windows.Forms.Label();
+            this.EspecialidadMedicoLabel = new System.Windows.Forms.Label();
+            this.NombreTB = new System.Windows.Forms.TextBox();
+            this.EspecialidadTB = new System.Windows.Forms.TextBox();
+            this.BotonLimpiarRoles = new System.Windows.Forms.Button();
+            this.CancelarBoton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AgendaTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CancelarBoton);
+            this.groupBox1.Controls.Add(this.BotonLimpiarRoles);
+            this.groupBox1.Controls.Add(this.EspecialidadTB);
+            this.groupBox1.Controls.Add(this.NombreTB);
+            this.groupBox1.Controls.Add(this.EspecialidadMedicoLabel);
+            this.groupBox1.Controls.Add(this.NombreMedicoLabel);
+            this.groupBox1.Controls.Add(this.AgendaTabla);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(501, 371);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seleccione la agenda a eliminar:";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // AgendaTabla
+            // 
+            this.AgendaTabla.AllowUserToAddRows = false;
+            this.AgendaTabla.AllowUserToDeleteRows = false;
+            this.AgendaTabla.AllowUserToResizeColumns = false;
+            this.AgendaTabla.AllowUserToResizeRows = false;
+            this.AgendaTabla.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.AgendaTabla.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.AgendaTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.AgendaTabla.Location = new System.Drawing.Point(6, 122);
+            this.AgendaTabla.Name = "AgendaTabla";
+            this.AgendaTabla.ReadOnly = true;
+            this.AgendaTabla.Size = new System.Drawing.Size(489, 186);
+            this.AgendaTabla.TabIndex = 1;
+            // 
+            // NombreMedicoLabel
+            // 
+            this.NombreMedicoLabel.AutoSize = true;
+            this.NombreMedicoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NombreMedicoLabel.Location = new System.Drawing.Point(17, 50);
+            this.NombreMedicoLabel.Name = "NombreMedicoLabel";
+            this.NombreMedicoLabel.Size = new System.Drawing.Size(111, 17);
+            this.NombreMedicoLabel.TabIndex = 2;
+            this.NombreMedicoLabel.Text = "Nombre Médico:";
+            this.NombreMedicoLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // EspecialidadMedicoLabel
+            // 
+            this.EspecialidadMedicoLabel.AutoSize = true;
+            this.EspecialidadMedicoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EspecialidadMedicoLabel.Location = new System.Drawing.Point(17, 82);
+            this.EspecialidadMedicoLabel.Name = "EspecialidadMedicoLabel";
+            this.EspecialidadMedicoLabel.Size = new System.Drawing.Size(92, 17);
+            this.EspecialidadMedicoLabel.TabIndex = 3;
+            this.EspecialidadMedicoLabel.Text = "Especialidad:";
+            // 
+            // NombreTB
+            // 
+            this.NombreTB.Location = new System.Drawing.Point(146, 47);
+            this.NombreTB.Name = "NombreTB";
+            this.NombreTB.Size = new System.Drawing.Size(320, 20);
+            this.NombreTB.TabIndex = 4;
+            // 
+            // EspecialidadTB
+            // 
+            this.EspecialidadTB.Location = new System.Drawing.Point(146, 79);
+            this.EspecialidadTB.Name = "EspecialidadTB";
+            this.EspecialidadTB.Size = new System.Drawing.Size(320, 20);
+            this.EspecialidadTB.TabIndex = 5;
+            // 
+            // BotonLimpiarRoles
+            // 
+            this.BotonLimpiarRoles.Location = new System.Drawing.Point(6, 327);
+            this.BotonLimpiarRoles.Name = "BotonLimpiarRoles";
+            this.BotonLimpiarRoles.Size = new System.Drawing.Size(95, 38);
+            this.BotonLimpiarRoles.TabIndex = 7;
+            this.BotonLimpiarRoles.Text = "Limpiar";
+            this.BotonLimpiarRoles.UseVisualStyleBackColor = true;
+            // 
+            // CancelarBoton
+            // 
+            this.CancelarBoton.BackColor = System.Drawing.Color.IndianRed;
+            this.CancelarBoton.Location = new System.Drawing.Point(397, 327);
+            this.CancelarBoton.Name = "CancelarBoton";
+            this.CancelarBoton.Size = new System.Drawing.Size(98, 38);
+            this.CancelarBoton.TabIndex = 8;
+            this.CancelarBoton.Text = "Cancelar";
+            this.CancelarBoton.UseVisualStyleBackColor = false;
+            // 
+            // BajaAP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 393);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BajaAP";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AgendaTabla)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label EspecialidadMedicoLabel;
+        private System.Windows.Forms.Label NombreMedicoLabel;
+        private System.Windows.Forms.DataGridView AgendaTabla;
+        private System.Windows.Forms.TextBox EspecialidadTB;
+        private System.Windows.Forms.TextBox NombreTB;
+        private System.Windows.Forms.Button BotonLimpiarRoles;
+        private System.Windows.Forms.Button CancelarBoton;
+    }
+}
---
+++ ClinicaFrba/ClinicaFrba/Abm Profesional/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ClinicaFrba.Abm_Profesional
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.VolverAgendaBoton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.EliminarAgendaLabel = new System.Windows.Forms.Label();
+            this.ModificarAgendaLabel = new System.Windows.Forms.Label();
+            this.SeleccionarOpcionAgendaLabel = new System.Windows.Forms.Label();
+            this.AgregarAgendaImagen = new System.Windows.Forms.PictureBox();
+            this.EliminarAgendaImagen = new System.Windows.Forms.PictureBox();
+            this.ModificarAgendaImagen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.AgregarAgendaImagen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EliminarAgendaImagen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ModificarAgendaImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(498, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Funcionalidad No Disponible";
+            // 
+            // VolverAgendaBoton
+            // 
+            this.VolverAgendaBoton.BackColor = System.Drawing.Color.IndianRed;
+            this.VolverAgendaBoton.Location = new System.Drawing.Point(602, 308);
+            this.VolverAgendaBoton.Name = "VolverAgendaBoton";
+            this.VolverAgendaBoton.Size = new System.Drawing.Size(85, 34);
+            this.VolverAgendaBoton.TabIndex = 17;
+            this.VolverAgendaBoton.Text = "Volver";
+            this.VolverAgendaBoton.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Agregar Agenda Profesional";
+            // 
+            // EliminarAgendaLabel
+            // 
+            this.EliminarAgendaLabel.AutoSize = true;
+            this.EliminarAgendaLabel.Location = new System.Drawing.Point(283, 221);
+            this.EliminarAgendaLabel.Name = "EliminarAgendaLabel";
+            this.EliminarAgendaLabel.Size = new System.Drawing.Size(138, 13);
+            this.EliminarAgendaLabel.TabIndex = 15;
+            this.EliminarAgendaLabel.Text = "Eliminar Agenda Profesional";
+            // 
+            // ModificarAgendaLabel
+            // 
+            this.ModificarAgendaLabel.AutoSize = true;
+            this.ModificarAgendaLabel.Location = new System.Drawing.Point(508, 221);
+            this.ModificarAgendaLabel.Name = "ModificarAgendaLabel";
+            this.ModificarAgendaLabel.Size = new System.Drawing.Size(145, 13);
+            this.ModificarAgendaLabel.TabIndex = 14;
+            this.ModificarAgendaLabel.Text = "Modificar Agenda Profesional";
+            // 
+            // SeleccionarOpcionAgendaLabel
+            // 
+            this.SeleccionarOpcionAgendaLabel.AutoSize = true;
+            this.SeleccionarOpcionAgendaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SeleccionarOpcionAgendaLabel.Location = new System.Drawing.Point(169, 30);
+            this.SeleccionarOpcionAgendaLabel.Name = "SeleccionarOpcionAgendaLabel";
+            this.SeleccionarOpcionAgendaLabel.Size = new System.Drawing.Size(373, 31);
+            this.SeleccionarOpcionAgendaLabel.TabIndex = 13;
+            this.SeleccionarOpcionAgendaLabel.Text = "Seleccione la opción deseada";
+            // 
+            // AgregarAgendaImagen
+            // 
+            this.AgregarAgendaImagen.Image = ((System.Drawing.Image)(resources.GetObject("AgregarAgendaImagen.Image")));
+            this.AgregarAgendaImagen.Location = new System.Drawing.Point(44, 78);
+            this.AgregarAgendaImagen.Name = "AgregarAgendaImagen";
+            this.AgregarAgendaImagen.Size = new System.Drawing.Size(161, 156);
+            this.AgregarAgendaImagen.TabIndex = 12;
+            this.AgregarAgendaImagen.TabStop = false;
+            // 
+            // EliminarAgendaImagen
+            // 
+            this.EliminarAgendaImagen.Image = ((System.Drawing.Image)(resources.GetObject("EliminarAgendaImagen.Image")));
+            this.EliminarAgendaImagen.Location = new System.Drawing.Point(269, 78);
+            this.EliminarAgendaImagen.Name = "EliminarAgendaImagen";
+            this.EliminarAgendaImagen.Size = new System.Drawing.Size(161, 156);
+            this.EliminarAgendaImagen.TabIndex = 11;
+            this.EliminarAgendaImagen.TabStop = false;
+            // 
+            // ModificarAgendaImagen
+            // 
+            this.ModificarAgendaImagen.Image = ((System.Drawing.Image)(resources.GetObject("ModificarAgendaImagen.Image")));
+            this.ModificarAgendaImagen.Location = new System.Drawing.Point(501, 78);
+            this.ModificarAgendaImagen.Name = "ModificarAgendaImagen";
+            this.ModificarAgendaImagen.Size = new System.Drawing.Size(161, 156);
+            this.ModificarAgendaImagen.TabIndex = 10;
+            this.ModificarAgendaImagen.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 349);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.VolverAgendaBoton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EliminarAgendaLabel);
+            this.Controls.Add(this.ModificarAgendaLabel);
+            this.Controls.Add(this.SeleccionarOpcionAgendaLabel);
+            this.Controls.Add(this.AgregarAgendaImagen);
+            this.Controls.Add(this.EliminarAgendaImagen);
+            this.Controls.Add(this.ModificarAgendaImagen);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.AgregarAgendaImagen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EliminarAgendaImagen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ModificarAgendaImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button VolverAgendaBoton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label EliminarAgendaLabel;
+        private System.Windows.Forms.Label ModificarAgendaLabel;
+        private System.Windows.Forms.Label SeleccionarOpcionAgendaLabel;
+        private System.Windows.Forms.PictureBox AgregarAgendaImagen;
+        private System.Windows.Forms.PictureBox EliminarAgendaImagen;
+        private System.Windows.Forms.PictureBox ModificarAgendaImagen;
+    }
+}
