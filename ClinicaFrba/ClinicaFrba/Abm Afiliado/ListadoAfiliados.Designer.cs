6e13a6a995e8375b4a177a2abe2a5dfcc39c403c
Nicolas <nicogon@Nicolass-MacBook.local>
2016-11-06 08:35:53 -0500

    agregado estructuras afiliados

---
+++ ClinicaFrba/ClinicaFrba/Abm Afiliado/ListadoAfiliados.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ClinicaFrba.AbmRol
+{
+    partial class ListadoRoles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tablaRoles = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btnfiltrar = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaRoles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tablaRoles
+            // 
+            this.tablaRoles.AllowUserToAddRows = false;
+            this.tablaRoles.AllowUserToDeleteRows = false;
+            this.tablaRoles.AllowUserToResizeColumns = false;
+            this.tablaRoles.AllowUserToResizeRows = false;
+            this.tablaRoles.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.tablaRoles.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.tablaRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaRoles.Location = new System.Drawing.Point(12, 114);
+            this.tablaRoles.Name = "tablaRoles";
+            this.tablaRoles.ReadOnly = true;
+            this.tablaRoles.Size = new System.Drawing.Size(366, 186);
+            this.tablaRoles.TabIndex = 0;
+            this.tablaRoles.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tablaRoles_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Funcionalidad";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Habilitado";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(181, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(197, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Habilitado",
+            "Inhabilitado"});
+            this.comboBox2.Location = new System.Drawing.Point(181, 39);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(197, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // btnfiltrar
+            // 
+            this.btnfiltrar.Location = new System.Drawing.Point(267, 66);
+            this.btnfiltrar.Name = "btnfiltrar";
+            this.btnfiltrar.Size = new System.Drawing.Size(111, 35);
+            this.btnfiltrar.TabIndex = 4;
+            this.btnfiltrar.Text = "Filtrar";
+            this.btnfiltrar.UseVisualStyleBackColor = true;
+            this.btnfiltrar.Click += new System.EventHandler(this.btnfiltrar_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Location = new System.Drawing.Point(12, 66);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(111, 35);
+            this.btnlimpiar.TabIndex = 5;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // ListadoRoles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 312);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btnfiltrar);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tablaRoles);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ListadoRoles";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Roles";
+            this.Load += new System.EventHandler(this.ListadoRoles_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tablaRoles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tablaRoles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btnfiltrar;
+        private System.Windows.Forms.Button btnlimpiar;
+    }
+}
---
+++ ClinicaFrba/ClinicaFrba/Abm Afiliado/ListadoAfiliados.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicaFrba.AbmRol
+{
+    public partial class ListadoRoles : Form
+    {
+        DataTable funcionalidades;
+
+        Accion accion;
+
+        public ListadoRoles()
+        {
+            InitializeComponent();
+        }
+
+        public ListadoRoles(Accion accionParametro)
+        {
+            InitializeComponent();
+            CargarTabla();
+            CargarFiltros();
+            AgregarBoton();
+            accion = accionParametro;
+
+        }
+        public void CargarFiltros() {
+
+
+            funcionalidades = DAO.DAORoles.listarFuncionalidades();
+            foreach (DataRow row in funcionalidades.Rows)
+            {
+                comboBox1.Items.Add(row["Nombre"].ToString());
+            }
+        
+        }
+
+        public void CargarTabla()
+        {
+            tablaRoles.DataSource = DAO.DAORoles.listarRoles();
+        }
+        private void ListadoRoles_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AgregarBoton()
+        {
+            DataGridViewButtonColumn btnColum = new DataGridViewButtonColumn();
+            btnColum.Name = "Seleccionar";
+            btnColum.Text = "Seleccionar";
+            btnColum.UseColumnTextForButtonValue = true;
+            tablaRoles.Columns.Insert(3, btnColum);
+       
+        }
+
+
+
+        private void tablaRoles_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+            if (Accion.Baja == accion)
+            {
+
+                DialogResult dialogResult = MessageBox.Show("Esta seguro?", "Esta seguro que quiere inhabilitar este rol?", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    DAO.DAORoles.inHabilitarRol(int.Parse(tablaRoles.Rows[e.RowIndex].Cells[1].Value.ToString()));
+                }
+          
+            }
+            if (Accion.Modificacion == accion){
+                new AltaModificacionRol(Accion.Modificacion,tablaRoles.Rows[e.RowIndex].Cells[1].Value.ToString()).ShowDialog();
+           
+            }
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            tablaRoles.DataSource = null;
+            comboBox1.Text = "";
+            comboBox2.Text="";
+
+        
+        }
+
+        private void btnfiltrar_Click(object sender, EventArgs e)
+        {
+            DataRow[] row = funcionalidades.Select("Nombre='"+comboBox1.Text+"'");
+            tablaRoles.Columns.Clear();
+            tablaRoles.DataSource = DAO.DAORoles.filtro(row.Count() > 0 ? int.Parse(row[0]["Funcionabilidad_id"].ToString()) : -1, comboBox2.Text);
+            AgregarBoton();
+
+        }
+    }
+}
