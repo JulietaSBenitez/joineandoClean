2fe35c550ce485d5ad04194aa71efddd443110d5
Nicolas <nicogon@Nicolass-MacBook.local>
2016-11-18 18:42:11 -0300

    Llenando los check box con los dias

--- ClinicaFrba/ClinicaFrba/Registrar Agenda Medico/AltaAP.cs
+++ ClinicaFrba/ClinicaFrba/Registrar Agenda Medico/AltaAP.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClinicaFrba.src;
+using System.Globalization;
 
 namespace ClinicaFrba.RegistrarAgendaMedico
 {
@@ -19,16 +20,86 @@ namespace ClinicaFrba.RegistrarAgendaMedico
             InitializeComponent();
             comboBox1.DataSource = medico.EspecialidadesSinAgenda();
             comboBox1.DisplayMember = "Nombre";
+            List<ComboBox> comboBoxListInicio = new List<ComboBox>();
+            List<ComboBox> comboBoxListFin = new List<ComboBox>();
+            comboBoxListInicio.Add(comboBoxInicioLunes);
+            comboBoxListInicio.Add(comboBoxInicioMartes);
+            comboBoxListInicio.Add(comboBoxInicioMiercoles);
+            comboBoxListInicio.Add(comboBoxInicioJueves);
+            comboBoxListInicio.Add(comboBoxInicioViernes);
+            comboBoxListFin.Add(comboBoxFinLunes);
+            comboBoxListFin.Add(comboBoxFinMartes);
+            comboBoxListFin.Add(comboBoxFinMiercoles);
+            comboBoxListFin.Add(comboBoxFinJueves);
+            comboBoxListFin.Add(comboBoxFinViernes);
+
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        private void CargarComboBoxRangoInicio(ComboBox comboBox, int horaInicio, int horaFin)
         {
-
+            DateTime time;
+            for ( int i = horaInicio; i <= horaFin; i++ )
+            {
+                time = new DateTime(1, 1, 1, i, 0, 0, 0);
+                comboBox.Items.Add(time.ToString("HH:mm"));
+                time = new DateTime(1, 1, 1, i, 30, 0, 0);
+                comboBox.Items.Add(time.ToString("HH:mm"));
+   
+            }
         }
 
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        private void CargarComboBoxRangoFin(ComboBox comboBox, int horaInicio, int horaFin)
         {
-            //TODO: MostrarDías
+            DateTime time;
+            for (int i = horaInicio; i <= horaFin; i++)
+            {
+                time = new DateTime(1, 1, 1, i, 30, 0, 0);
+                comboBox.Items.Add(time.ToString("HH:mm"));
+                time = new DateTime(1, 1, 1, i + 1, 0, 0, 0);
+                comboBox.Items.Add(time.ToString("HH:mm"));
+
+            }
         }
+
+        private void CargarTodosLosComboBoxInicio( ) {
+
+            // Acá quiero hacer algo asi: 
+            //comboBoxListInicio.forEach[unComboBox | CargarComboBoxRangoInicio(unComboBox, 7, 20)]
+
+            CargarComboBoxRangoFin(comboBoxInicioSabado, 10, 15);
+
+            comboBoxInicioLunes.Enabled = false;
+            comboBoxInicioMartes.Enabled = false;
+            comboBoxInicioMiercoles.Enabled = false;
+            comboBoxInicioJueves.Enabled = false;
+            comboBoxInicioViernes.Enabled = false;
+            comboBoxInicioSabado.Enabled = false;
+        }
+
+        private void CargarTodosLosComboBoxInicio(){
+
+            // Acá quiero hacer algo asi: 
+            //comboBoxListFin.forEach[unComboBox | unComboBox.CargarComboBoxRangoFin(unComboBox, 7, 20)]
+
+            CargarComboBoxRangoFin(comboBoxFinSabado, 10, 15);
+            
+            comboBoxFinLunes.Enabled = false;
+            comboBoxFinMartes.Enabled = false;
+            comboBoxFinMiercoles.Enabled = false;
+            comboBoxFinJueves.Enabled = false;
+            comboBoxFinViernes.Enabled = false;
+            comboBoxFinSabado.Enabled = false;
+        }
+
+        private void botonVolverAgenda_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        }
+
+        }
+
     }
-}
