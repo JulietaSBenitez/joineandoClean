fd0e8a7e469f7ec2cfe5f612e53f3eea1dd9bd1c
Nicolas <nicogon@Nicolass-MacBook.local>
2016-11-20 10:02:22 -0300

    refactor afiliados

---
+++ ClinicaFrba/ClinicaFrba/Abm Afiliado/AltaModificacionAfiliados.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicaFrba.Abm_Afiliado
+{
+    public partial class AltaModificacionAfiliados : Form
+    {
+        private Accion accion;
+
+        public AltaModificacionAfiliados()
+        {
+            InitializeComponent();
+        }
+
+        public AltaModificacionAfiliados(Accion accion)
+        {
+            // TODO: Complete member initialization
+            this.accion = accion;
+        }
+    }
+}
