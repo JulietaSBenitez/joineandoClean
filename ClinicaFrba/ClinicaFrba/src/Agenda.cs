fbe080eca2e59fa74be1014baf90b6ce88b7084d
MagdalenaMileto <maggiemileto@gmail.com>
2016-11-20 17:50:51 -0300

    Stored Procedures
    Tengo que hacer dos SP, pero me tengo que ir y quería commitear por las dudas. :3

---
+++ ClinicaFrba/ClinicaFrba/src/Agenda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaFrba.src
+{
+    class Agenda
+    {
+
+        public int CantidadDeHorasTrabajadas() { 
+            return 2}
+
+    }
+}
--- ClinicaFrba/ClinicaFrba/src/Medico.cs
+++ ClinicaFrba/ClinicaFrba/src/Medico.cs
@@ -12,10 +12,19 @@ namespace ClinicaFrba.src
     {
         public int ID { get; set; }
         public List<Especialidad> Especialidades { get; set; }
+        public List<Agenda> Agendas { get; set; }
 
         public List<Especialidad> EspecialidadesSinAgenda()
         {
             return Especialidades.FindAll(especialidad => !especialidad.YaTieneAgenda(this));
         }
+
+        public bool TrabajaMasDe48Horas()
+        {
+            return CantidadDeHorasTrabajadas() > 48;
+        }
+
+        public int CantidadDeHorasTrabajadas() { 
+            return Agendas.Sum(agenda => agenda.CantidadDeHorasTrabajadas())}
     }
 }
