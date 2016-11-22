b288433b49b4797c2aa7b4810e3c61c8311ff74c
MagdalenaMileto <maggiemileto@gmail.com>
2016-11-22 16:23:14 -0300

    Cancelar Turnos Para Un Dia
    Pantalla.

---
+++ ClinicaFrba/ClinicaFrba/Cancelar Atencion/CancelarTurnoProfesionalDia.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ClinicaFrba.Cancelar_Atencion
+{
+    partial class CancelarTurnoProfesionalDia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.CancelarTurnosButton = new System.Windows.Forms.Button();
+            this.VolverButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.VolverButton);
+            this.groupBox1.Controls.Add(this.CancelarTurnosButton);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.monthCalendar1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(332, 312);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Seleccione Día";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(67, 56);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 7;
+            // 
+            // CancelarTurnosButton
+            // 
+            this.CancelarTurnosButton.Location = new System.Drawing.Point(98, 271);
+            this.CancelarTurnosButton.Name = "CancelarTurnosButton";
+            this.CancelarTurnosButton.Size = new System.Drawing.Size(105, 30);
+            this.CancelarTurnosButton.TabIndex = 1;
+            this.CancelarTurnosButton.Text = "Cancelar Turnos";
+            this.CancelarTurnosButton.UseVisualStyleBackColor = true;
+            // 
+            // VolverButton
+            // 
+            this.VolverButton.BackColor = System.Drawing.Color.IndianRed;
+            this.VolverButton.Location = new System.Drawing.Point(217, 271);
+            this.VolverButton.Name = "VolverButton";
+            this.VolverButton.Size = new System.Drawing.Size(105, 30);
+            this.VolverButton.TabIndex = 9;
+            this.VolverButton.Text = "Volver";
+            this.VolverButton.UseVisualStyleBackColor = false;
+            // 
+            // CancelarTurnoProfesionalDia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 338);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CancelarTurnoProfesionalDia";
+            this.Text = "Cancelar Turno de un dia";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button CancelarTurnosButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button VolverButton;
+    }
+}
