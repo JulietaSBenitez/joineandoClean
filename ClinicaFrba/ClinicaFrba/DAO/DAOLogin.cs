48b4092bce57cc237ae2c4b5eba830808c9490a7
Nicolas <nicogon@Nicolass-MacBook.local>
2016-11-29 12:03:02 -0300

    Arreglando para que no me rompa todo

---
+++ ClinicaFrba/ClinicaFrba/DAO/DAOLogin.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaFrba.DAO
+{
+    class DAOLogin
+    {
+
+
+        public static DataTable roles_x_usuario(int userId) {
+
+            DataTable data = new DataTable();
+            SQLHelper.ConnectionValue = Properties.Settings.Default.conector;
+            SQLHelper.CreateObjects(true);
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Clear();
+            SqlParameter parameter;
+
+            try
+            {
+
+                parameter = new SqlParameter("@UserId", SqlDbType.Int);
+                parameter.Value = userId;
+                parameters.Add(parameter);
+
+              
+
+                data = SQLHelper.SQLHelper_ExecuteReader("JOINEANDO_ANDO.roles_x_usuario", parameters);
+
+                return data;
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                SQLHelper.RollBackTransction();
+                throw ex;
+            }
+            finally
+            {
+
+                SQLHelper.ClearObjects();
+            }
+
+
+        
+        }
+
+
+
+
+        public static int login(string username, string password)
+        {
+
+            DataTable data = new DataTable();
+            SQLHelper.ConnectionValue = Properties.Settings.Default.conector;
+            SQLHelper.CreateObjects(true);
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Clear();
+            SqlParameter parameter;
+
+            try
+            {
+
+                parameter = new SqlParameter("@UserId", SqlDbType.NVarChar, 50);
+                parameter.Value = username;
+                parameters.Add(parameter);
+
+                parameter = new SqlParameter("@Pas", SqlDbType.NVarChar, 50);
+                parameter.Value = password;
+                parameters.Add(parameter);
+
+                data = SQLHelper.SQLHelper_ExecuteReader("JOINEANDO_ANDO.validar_usuario", parameters);
+
+                return int.Parse(data.Rows[0][1].ToString());
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                SQLHelper.RollBackTransction();
+                throw ex;
+            }
+            finally
+            {
+               
+                SQLHelper.ClearObjects();
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ClinicaFrba/ClinicaFrba/DAO/QueryAdapterMaggie.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ClinicaFrba.Properties;
+using System.Data;
+using ClinicaFrba.src;
+
+namespace ClinicaFrba.DAO
+{
+    class QueryAdapterMaggie
+    {
+
+        private static string infoConexion()
+        {
+            return Properties.Settings.Default.conector;
+
+        }
+
+        private static SqlConnection conexion()
+        {
+            SqlConnection conexion = new SqlConnection(infoConexion());
+            return conexion;
+        }
+
+        public static List<DataRow> ejecutarSP(string nombreStoredProcedure, params SqlParameter[] parametros)
+        {
+
+            DataTable tabla = new DataTable();
+            SqlConnection conexion = QueryAdapterMaggie.conexion();
+            SqlCommand storedProcedure = new SqlCommand("JOINEANDO_ANDO." + nombreStoredProcedure, conexion);
+            storedProcedure.CommandType = CommandType.StoredProcedure;
+
+            if (parametros != null)
+            {
+                storedProcedure.Parameters.AddRange(parametros.ToArray());
+            }
+
+            SqlDataAdapter adaptador = new SqlDataAdapter(storedProcedure);
+
+            adaptador.Fill(tabla);
+
+            return tabla.AsEnumerable().ToList();
+
+
+        }
+
+        public static bool ejecutarSPBooleano(string nombreStoredProcedure, params SqlParameter[] parametros)
+        {
+
+            List<DataRow> filas = ejecutarSP(nombreStoredProcedure, parametros);
+            return Convert.ToBoolean(filas.First()[0]);
+
+        }
+
+        public static int ejecutarSPEntero(string nombreStoredProcedure, params SqlParameter[] parametros)
+        {
+
+            List<DataRow> filas = ejecutarSP(nombreStoredProcedure, parametros);
+            return Convert.ToInt32(filas.First()[0]);
+
+        }
+    }
+}
