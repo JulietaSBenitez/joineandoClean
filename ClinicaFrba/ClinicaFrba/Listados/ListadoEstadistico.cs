bb2f55972abf8de92ecbe10bc385a20682208511
Nicolas <nicogon@Nicolass-MacBook.local>
2016-11-29 11:58:43 -0300

    asd

--- ClinicaFrba/ClinicaFrba/Listados/ListadoEstadistico.cs
+++ ClinicaFrba/ClinicaFrba/Listados/ListadoEstadistico.cs
@@ -15,8 +15,14 @@ namespace ClinicaFrba.Listados
         public ListadoEstadistico()
         {
             InitializeComponent();
-
             semestre.SelectedIndex = 0;
+            especialidad.DataSource = DAO.DAOListado.especialidades();
+            especialidad.ValueMember = "Nombre_Especialidad";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
 
 
@@ -31,53 +37,64 @@ namespace ClinicaFrba.Listados
 
         private void mostrar_Click(object sender, EventArgs e)
         {
+            /*
+             * 
+             * 
+             *
 
+
+
+
+             * 
+            */
             if (IsNumeric(ano.Text) == false)
             {
                 MessageBox.Show("Ingrese un ano valido", "Listado Estadistico", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 return;
             }
             int anio = int.Parse(ano.Text);
-            int ssemestre = 2;
-            if (semestre.Text == "Primero") ssemestre = 1;
+            int ssemestre=2;
+            if(semestre.Text=="Primero") ssemestre=1;
 
-            switch (Acciones.Text)
-            {
-
+            switch (acciones.Text) { 
+         
                 case "":
-                    MessageBox.Show("Seleccione un listado estadistico", "Listado Estadistico", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-
+                    MessageBox.Show("Seleccione un listado estadistico","Listado Estadistico",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+     
                     break;
                 case "Top 5 de las especialidades con mas cancelaciones":
-                    TablaListado.DataSource = DAO.DAOListado.ListadoEspecialidadesConMasCancelaciones(anio, ssemestre);
-                    break;
+                  tablaListado.DataSource=  DAO.DAOListado.ListadoEspecialidadesConMasCancelaciones( anio, ssemestre );
+                break;
 
                 case "Top 5 de los profesionales más consultados por Plan":
 
-                    TablaListado.DataSource = DAO.DAOListado.ListadoProfesionalesMasConsultadosPorPlan(anio, ssemestre);
-
-                    break;
+                tablaListado.DataSource = DAO.DAOListado.ListadoProfesionalesMasConsultadosPorPlan(anio, ssemestre);
+                   
+                break;
 
                 case "Top 5 de los profesionales con menos horas trabajadas":
-                    TablaListado.DataSource = DAO.DAOListado.ListadoProfesionalesConMenosHorasTrabajadas(anio, ssemestre);
-
-                    break;
+                     tablaListado.DataSource=  DAO.DAOListado.ListadoProfesionalesConMenosHorasTrabajadas( anio, ssemestre );
+                    
+                break;
 
                 case "Top 5 de los afiliados con mayor cantidad de bonos comprados":
 
-                    TablaListado.DataSource = DAO.DAOListado.ListadoAfiliadosConMayorBonosComprados(anio, ssemestre);
-
-                    break;
+                tablaListado.DataSource = DAO.DAOListado.ListadoAfiliadosConMayorBonosComprados(anio, ssemestre);
+                   
+                break;
 
                 case "Top 5 de las especialidades con más bonos utilizados":
-                    TablaListado.DataSource = DAO.DAOListado.ListadoEspecialidadesConMasBonosUtilizados(anio, ssemestre);
-
-
-                    break;
+                tablaListado.DataSource = DAO.DAOListado.ListadoEspecialidadesConMasBonosUtilizados(anio, ssemestre);
+                   
+                    
+                break;
 
             }
         }
 
+        private void ListadoEstadistico_Load(object sender, EventArgs e)
+        {
 
+        }
     }
 }
