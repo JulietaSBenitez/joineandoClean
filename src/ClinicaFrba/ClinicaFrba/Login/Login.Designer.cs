7586da1eaa4cfcb50bd0e6c0b9c983d2aa23ec9c
Nicolas <nicogon@Nicolass-MacBook.local>
2016-11-30 02:11:39 -0300

    login

--- src/ClinicaFrba/ClinicaFrba/Login/Login.Designer.cs
+++ src/ClinicaFrba/ClinicaFrba/Login/Login.Designer.cs
@@ -33,9 +33,6 @@
             this.pass = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -55,7 +52,6 @@
             this.usuario.Name = "usuario";
             this.usuario.Size = new System.Drawing.Size(193, 26);
             this.usuario.TabIndex = 1;
-            this.usuario.Text = "admin";
             this.usuario.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // pass
@@ -63,9 +59,9 @@
             this.pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pass.Location = new System.Drawing.Point(127, 44);
             this.pass.Name = "pass";
+            this.pass.PasswordChar = '*';
             this.pass.Size = new System.Drawing.Size(193, 26);
             this.pass.TabIndex = 2;
-            this.pass.Text = "w23e";
             // 
             // label1
             // 
@@ -87,44 +83,11 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Contrasena";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(32, 120);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 47);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "admin";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(113, 120);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 47);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "paciente";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(194, 120);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 47);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "medico";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 183);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(332, 126);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pass);
@@ -145,8 +108,5 @@
         private System.Windows.Forms.TextBox pass;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
